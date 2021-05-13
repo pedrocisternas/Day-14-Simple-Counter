4b88c1f5529c3155534c5fcbf28f8b087b3265f6
pedrocisternas <pedrocisternas86@gmail.com>
2021-05-13 00:51:23 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,44 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
+export function Home(props) {
+	const [userNum, setUserNum] = useState(null);
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div>
+				<button
+					onClick={() => {
+						props.myFunction(userNum);
+					}}>
+					Send
+				</button>
+				<input
+					onChange={e => {
+						setUserNum(e.target.value);
+					}}
+					type="number"
+					placeholder="Pick a 6 digit number"
+				/>
+			</div>
+			<div className="container d-flex">
+				<div>{props.number1}</div>
+				<div>{props.number2}</div>
+				<div>{props.number3}</div>
+				<div>{props.number4}</div>
+				<div>{props.number5}</div>
+				<div>{props.number6}</div>
+			</div>
+		</>
 	);
 }
+
+Home.propTypes = {
+	number1: PropTypes.string,
+	number2: PropTypes.string,
+	number3: PropTypes.string,
+	number4: PropTypes.string,
+	number5: PropTypes.string,
+	number6: PropTypes.string,
+	myFunction: PropTypes.func
+};
--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,26 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let num = 0;
+function setNum(number) {
+	num = parseInt(number);
+}
+function counter() {
+	num += 1;
+	let string = "00000" + num;
+	let num2 = string.slice(-6);
+
+	ReactDOM.render(
+		<Home
+			number1={num2.charAt(0)}
+			number2={num2.charAt(1)}
+			number3={num2.charAt(2)}
+			number4={num2.charAt(3)}
+			number5={num2.charAt(4)}
+			number6={num2.charAt(5)}
+			myFunction={setNum}
+		/>,
+		document.querySelector("#app")
+	);
+}
+setInterval(counter, 1000);
