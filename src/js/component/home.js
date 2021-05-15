e971474c929c2699b4dcdf154c9296397e6f133b
pedrocisternas <pedrocisternas86@gmail.com>
2021-05-15 03:11:09 +0000

    updated final

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,28 +6,46 @@ export function Home(props) {
 	const [userNum, setUserNum] = useState(null);
 	return (
 		<>
-			<div>
+			<div className="container-fluid d-flex justify-content-center">
 				<button
+					className="btn btn-danger btn-lg"
+					onClick={() => {
+						props.setRun();
+					}}>
+					Stop / Resume
+				</button>
+				<button
+					className="btn btn-primary btn-lg"
+					onClick={() => {
+						props.myFunction(0);
+					}}>
+					Reset
+				</button>
+				<button
+					className="btn btn-success btn-lg"
 					onClick={() => {
 						props.myFunction(userNum);
 					}}>
-					Send
+					Set
 				</button>
+			</div>
+			<div className="container-fluid d-flex justify-content-center input-group input-group-lg m-2">
 				<input
 					onChange={e => {
 						setUserNum(e.target.value);
 					}}
 					type="number"
 					placeholder="Pick a 6 digit number"
+					aria-label="Large"
 				/>
 			</div>
-			<div className="container d-flex">
-				<div>{props.number1}</div>
-				<div>{props.number2}</div>
-				<div>{props.number3}</div>
-				<div>{props.number4}</div>
-				<div>{props.number5}</div>
-				<div>{props.number6}</div>
+			<div className="container-fluid d-flex justify-content-center ml-2">
+				<h1>{props.number1}</h1>
+				<h1>{props.number2}</h1>
+				<h1>{props.number3}</h1>
+				<h1>{props.number4}</h1>
+				<h1>{props.number5}</h1>
+				<h1>{props.number6}</h1>
 			</div>
 		</>
 	);
@@ -40,5 +58,7 @@ Home.propTypes = {
 	number4: PropTypes.string,
 	number5: PropTypes.string,
 	number6: PropTypes.string,
-	myFunction: PropTypes.func
+	myFunction: PropTypes.func,
+	setRun: PropTypes.func,
+	isRunning: PropTypes.bool
 };
--- src/js/index.js
+++ src/js/index.js
@@ -13,25 +13,36 @@ import { Home } from "./component/home.js";
 
 //render your react application
 let num = 0;
+let run = true;
+
+function setRun() {
+	run = !run;
+}
+
 function setNum(number) {
 	num = parseInt(number);
 }
-function counter() {
-	num += 1;
-	let string = "00000" + num;
-	let num2 = string.slice(-6);
 
-	ReactDOM.render(
-		<Home
-			number1={num2.charAt(0)}
-			number2={num2.charAt(1)}
-			number3={num2.charAt(2)}
-			number4={num2.charAt(3)}
-			number5={num2.charAt(4)}
-			number6={num2.charAt(5)}
-			myFunction={setNum}
-		/>,
-		document.querySelector("#app")
-	);
+function counter() {
+	if (run) {
+		num += 1;
+		let string = "00000" + num;
+		let num2 = string.slice(-6);
+
+		ReactDOM.render(
+			<Home
+				number1={num2.charAt(0)}
+				number2={num2.charAt(1)}
+				number3={num2.charAt(2)}
+				number4={num2.charAt(3)}
+				number5={num2.charAt(4)}
+				number6={num2.charAt(5)}
+				myFunction={setNum}
+				setRun={setRun}
+				isRunning={run}
+			/>,
+			document.querySelector("#app")
+		);
+	}
 }
 setInterval(counter, 1000);
